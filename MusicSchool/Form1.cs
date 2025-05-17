302f5a5b91b6577783885961caec2f04643344cd
Виталий Кертанов <sasawawa2033@gmail.com>
2025-05-17 21:45:05 +0300

    Добавьте файлы проекта.

---
+++ MusicSchool/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MusicSchool
+{
+    public partial class Form1 : Form
+    {
+        private MusicSchoolController _controller;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            var model = new MusicSchool();
+            var view = new MusicSchoolControl(model, this);
+            _controller = new MusicSchoolController(model, view);
+
+            Controls.Add(view);
+            this.Load += async (s, e) => await _controller.StartLessons();
+        }
+    }
+}
---
+++ MusicSchool/MusicSchoolController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicSchool
+{
+    public class MusicSchoolController
+    {
+        private MusicSchool _model;
+        private MusicSchoolControl _view;
+
+        public MusicSchoolController(MusicSchool model, MusicSchoolControl view)
+        {
+            _model = model;
+            _view = view;
+            _model.Subscribe(view);
+        }
+
+        public async Task StartLessons()
+        {
+            await _model.StartMusicLessons();
+        }
+    }
+}
